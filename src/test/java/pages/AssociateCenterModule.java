7d6cafb27fd0236db9c872d32e30a853cd305b62
KURELLAVISHAL <kurellavishal@gmail.com>
2018-11-02 18:53:20 +0530

    firstCommit

--- src/test/java/pages/AssociateCenterModule.java
+++ src/test/java/pages/AssociateCenterModule.java
@@ -31,51 +31,114 @@ public class AssociateCenterModule implements BaseTest
 	public WebElement masterData;
 	
 	@FindBy(xpath="(//*[text()='Employee'])[1]")
-	public WebElement employee;
+	public WebElement masterDataEmployee;
 	
 	@FindBy(xpath="(//*[text()='Consultant'])[1]")
-	public WebElement consultant;
+	public WebElement masterDataConsultant;
 	
 	
 	@FindBy(xpath="(//*[text()='Contract Staff'])[1]")
-	public WebElement contractStaff;
+	public WebElement masterDataContractStaff;
 	
 	@FindBy(xpath="(//*[text()='CHFs/CHEs/BCPs'])[1]")
-	public WebElement CHFsCHEsBCPs;
+	public WebElement masterDataCHFsCHEsBCPs;
 	
 	@FindBy(xpath="(//*[text()='Program Associate'])[1]")
-	public WebElement programAssociate;
+	public WebElement masterDataProgramAssociate;
 	
 	@FindBy(xpath="(//*[text()='Volunteers'])[1]")
-	public WebElement volunteers;
+	public WebElement masterDataVolunteers;
 	
 	
 	@FindBy(xpath="(//*[text()='Student'])[1]")
-	public WebElement student;
+	public WebElement masterDataStudent;
 	
 	@FindBy(xpath="(//*[text()='CHFMapping'])[1]")
-	public WebElement chfMapping;
+	public WebElement masterDataCHFMapping;
 	
 	@FindBy(xpath="(//*[text()='Personnel Expenses'])[1]")
 	public WebElement personnelExpenses;
 	
 	@FindBy(xpath="(//*[text()='Salaries'])[1]")
-	public WebElement salaries;
+	public WebElement personnelExpensesSalaries;
 	
 	@FindBy(xpath="(//*[text()='Fees'])[1]")
-	public WebElement fees;
+	public WebElement personnelExpensesFees;
 	
 	@FindBy(xpath="(//*[text()='Associate Program Fees'])[1]")
-	public WebElement associateProgramFees;
+	public WebElement personnelExpensesAssociateProgramFees;
 	
 	@FindBy(xpath="(//*[text()='Staff Advances ( Salary Advances)'])[1]")
-	public WebElement staffAdvances;
+	public WebElement personnelExpensesStaffAdvances;
 	
 	@FindBy(xpath="(//*[text()='Payments & Settlements'])[1]")
-	public WebElement personalExpensesPaymentAndSettlements;
+	public WebElement personnelExpensesPaymentAndSettlements;
 	
-	//@FindBy(xpath="")
-	//public WebElement ;
+	@FindBy(xpath="(//*[text()='Non Personnel Expenses'])[1]")
+	public WebElement nonPersonnelExpenses;
+	
+	@FindBy(xpath="(//*[text()='Advances'])[1]")
+	public WebElement nonPersonnelExpensesAdvance;
+	
+	@FindBy(xpath="(//*[text()='Expenses Booking'])[1]")
+	public WebElement nonPersonnelExpensesBooking;
+	
+	@FindBy(xpath="(//*[text()='Payments & Settlements'])[2]")
+	public WebElement nonPersonnelExpensesPaymentAndSettlements;
+	
+	@FindBy(xpath="(//*[text()='Reimbursements & DirectPayments'])[1]")
+	public WebElement nonPersonnelExpensesReimbursementsAndDirectPayments;
+	
+	@FindBy(xpath="(//*[text()='Program Expenses'])[1]")
+	public WebElement programExpenses;
+	
+	@FindBy(xpath="(//*[text()='Advances'])[2]")
+	public WebElement programExpensesAdvances;
+	
+	@FindBy(xpath="(//*[text()='Program Expenses'])[2]")
+	public WebElement programExpensesInProgramExpenses;
+	
+	@FindBy(xpath="(//*[text()='SNP Advances'])[1]")
+	public WebElement programExpensesSNPAdvances;
+	
+	@FindBy(xpath="(//*[text()='SNP FEE'])[1]")
+	public WebElement programExpensesSNPFee;
+	
+	@FindBy(xpath="(//*[text()='Payments & Settlements'])[3]")
+	public WebElement programExpensesPaymentAndSettlements;
+	
+	@FindBy(xpath="(//*[text()='Stipends'])[1]")
+	public WebElement programExpensesStipends;
+	
+	@FindBy(xpath="(//*[text()='CHFIncentive'])[1]")
+	public WebElement programExpensesCHFIncentive;
+	
+	@FindBy(xpath="(//*[text()='Stipends Payments'])[1]")
+	public WebElement programExpensesStipendsPayments;
+	
+	
+	@FindBy(xpath="(//*[text()='Volunteers'])[2]")
+	public WebElement programExpensesVolunteers;
+	
+	
+	@FindBy(xpath="(//*[text()='Volunteers Payments'])[1]")
+	public WebElement programExpensesVolunteersPayments;
+	
+	
+	@FindBy(xpath="(//*[text()='Receipts'])[1]")
+	public WebElement receipts;
+	
+	
+	@FindBy(xpath="(//*[text()='Payment Recovery '])[1]")
+	public WebElement receiptsPaymentRecovery;
+	
+	
+	@FindBy(xpath="(//*[text()='Associate Receipts '])[1]")
+	public WebElement receiptsAssociateReceipts;
+	
+	@FindBy(xpath="(//*[text()='Status'])[1]")
+	public WebElement status;
+	
 	
 	
 	
@@ -92,18 +155,8 @@ public class AssociateCenterModule implements BaseTest
 		clickObj.click(associateCenter);
 	}
 	
-	public void clickMasterData()
-	{
-		
-		
-		clickObj.click(masterData);
-		ExplicitWaitMethods.waitForVisibility(chfMapping);
-		clickObj.click(chfMapping);
-	}
 	
 	
-	
-
 
 	
 }
